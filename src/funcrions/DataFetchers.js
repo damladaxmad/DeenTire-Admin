61dd12becaa12baa417208ac644f47c3c064452b
unknown <damladaxmad@gmail.com>
2023-03-04 19:59:33 +0300

    securing stuff

--- src/funcrions/DataFetchers.js
+++ src/funcrions/DataFetchers.js
@@ -8,8 +8,8 @@ const useFetch = (url, change = "hi", name) => {
             const response = await axios
             .get(`${constants.baseUrl}/${url}`, {
               headers: {
-                'authorization': "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYjE5N2IzN2Y1ZjE2NzQxMDliZjIyYSIsImlhdCI6MTY3Nzk0NTEyOSwiZXhwIjoxNjgwNTM3MTI5fQ.eN83KKn3-NQgeTaMIWIbMGt2CVXTvx3ln44zoH5pLB8"
-                },
+                'authorization': constants.token
+               },
             }).then((res)=>{
                 setData(res.data.data[name])
             })
